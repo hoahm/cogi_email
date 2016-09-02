92031afcc66747e178020d81ac9683d06ad2dedc
Hoang Hoa <nobi.hoa@gmail.com>
2016-09-02 21:30:06 +0700

    Validate email address

--- lib/cogi_email.rb
+++ lib/cogi_email.rb
@@ -4,4 +4,28 @@ module CogiEmail
   def self.hi
     puts "Hello from Cogi::Email"
   end
+
+  # Check if a string is a valid email address.
+  #
+  # This library is so strict to ensure that all email is clean and deliverable.
+  #
+  # References:
+  #   https://www.jochentopf.com/email/
+  #   https://fightingforalostcause.net/content/misc/2006/compare-email-regex.php
+  #
+  # @param [String] email Email address
+  #
+  # @return [Boolean] True if it is in a valid email, otherwise False
+  #
+  # @example
+  #
+  #   CogiEmail.validate?('peter_brown@example.com') # => true
+  #   CogiEmail.validate?('peter-brown@example.com') # => true
+  #   CogiEmail.validate?('peter.brown@example.com') # => true
+  #   CogiEmail.validate?('peter brown@example.com') # => false
+  #   CogiEmail.validate?('peter_brown@@example.com') # => false
+  def self.validate?(email)
+    pattern = /\A\s*([-\p{L}\d+._]{1,64})@((?:[-\p{L}\d]+\.)+\p{L}{2,})\s*\z/i
+    !!(email =~ pattern)
+  end
 end
---
+++ spec/cogi_email_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+describe CogiEmail do
+  subject { CogiEmail.new }
+
+  describe '#hi' do
+    it 'return nil' do
+      expect(CogiEmail.hi).to eq(nil)
+    end
+  end
+
+  describe '#validate?' do
+    context 'valid email address' do
+      [
+        "a+b@plus-in-local.com",
+        "a_b@underscore-in-local.com",
+        "user@example.com",
+        " user@example.com ",
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
+        "01234567890@numbers-in-local.net",
+        "a@single-character-in-local.org",
+        "one-character-third-level@a.example.com",
+        "single-character-in-sld@x.org",
+        "local@dash-in-sld.com",
+        "letters-in-sld@123.com",
+        "one-letter-sld@x.org",
+        "uncommon-tld@sld.museum",
+        "uncommon-tld@sld.travel",
+        "uncommon-tld@sld.mobi",
+        "country-code-tld@sld.uk",
+        "country-code-tld@sld.rw",
+        "local@sld.newTLD",
+        "local@sub.domains.com",
+        "aaa@bbb.co.jp",
+        "nigel.worthington@big.co.uk",
+        "f@c.com",
+        "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca",
+        "ящик@яндекс.рф",
+        "joe+ruby-mail@example.com"
+      ].each do |email|
+        it "#{email.inspect} should be valid" do
+          expect(CogiEmail.validate?(email)).to be_truthy
+        end
+      end
+    end
+
+    context 'invalid email address' do
+      [
+        "",
+        "f@s",
+        "f@s.c",
+        "@bar.com",
+        "test@example.com@example.com",
+        "test@",
+        "@missing-local.org",
+        "a b@space-in-local.com",
+        "! \#$%\`|@invalid-characters-in-local.org",
+        "<>@[]\`|@even-more-invalid-characters-in-local.org",
+        "missing-sld@.com",
+        "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
+        "missing-dot-before-tld@com",
+        "missing-tld@sld.",
+        " ",
+        "missing-at-sign.net",
+        "unbracketed-IP@127.0.0.1",
+        "invalid-ip@127.0.0.1.26",
+        "another-invalid-ip@127.0.0.256",
+        "IP-and-port@127.0.0.1:25",
+        "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
+        "user@example.com\n<script>alert('hello')</script>"
+      ].each do |email|
+        it "#{email.inspect} should not be valid" do
+          expect(CogiEmail.validate?(email)).to be_falsey
+        end
+      end
+    end
+  end
+end
